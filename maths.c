c11accd36386f3a7860c9a421de281884d508d81
Jordan Burgess <P15184751@GWHXX680WIN017W.lec-admin.dmu.ac.uk>
2017-01-31 15:45:11 +0000

    Maths functions y'all

--- maths.c
+++ maths.c
@@ -27,6 +27,33 @@ int main() {
 	printf("%i\n", add(a, b));
 	printf("%i\n", subtract(a, b));
 	printf("%i\n", multiply(a, b));
+	printf("%i\n", divide(a, b));
+	printf("\n");
+	
+	int * arr;
+	int x, n = 0;
+	int sum = 0;
+	
+	while(n == 0) {
+		printf("Enter a number for the size of the array: ");
+		scanf(" %i", &n);
+		if(n > 5 || n < 1) {
+			printf("Invalid number for array\n");
+			n = 0;
+		}
+	}
+
+	arr = (int *)malloc(n * sizeof(int));
+	
+	for(x = 0; x < n; x++) {
+		printf("Enter the number at index %i: ", x);
+		scanf(" %i", &arr[x]);
+		sum = sum + arr[x];
+	}
+	
+	printf("The average of the numbers is %i\n", sum/n);
+	
+	free(arr);
 	
 	return 0;
 }
